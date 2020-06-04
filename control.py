d7c306b3c7b03e20b18864871db4337ebd07adf9
PARAPAuk <parapa@gmail.com>
2020-06-04 03:47:15 +0100

    Playing with the on-off system

--- control.py
+++ control.py
@@ -17,31 +17,32 @@ import os
 from pathlib import Path
 logging.basicConfig(filename='/fan/fan.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
+
+
+log = '/fan/log/log.log'
+# target temp
+temp = 46
+# how many breaches of the temp before turning fan on? (higher = longer)
+tries = 5
+temp = int(temp) * 0.935
+multi = 100 / (int(tries) + 2)
+i = 0
+off = 0
+status = 'off'
+targetFilePath = '/sys/class/gpio/gpio2/value'
+fan = 2
+led = 16
+
+
+
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(led, GPIO.OUT)
 
-GPIO.output(16, GPIO.HIGH)
-time.sleep(0.2)
-GPIO.output(16, GPIO.LOW)
-time.sleep(0.2)
-GPIO.output(16, GPIO.HIGH)
-time.sleep(0.2)
-GPIO.output(16, GPIO.LOW)
-time.sleep(0.2)
-GPIO.output(16, GPIO.HIGH)
-time.sleep(0.2)
-GPIO.output(16, GPIO.LOW)
-time.sleep(0.2)
-GPIO.output(16, GPIO.HIGH)
-time.sleep(0.2)
-GPIO.output(16, GPIO.LOW)
-time.sleep(0.2)
-GPIO.output(16, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(16, GPIO.LOW)
-time.sleep(0.2)
-brightness = GPIO.PWM(16, 100)
+brightness = GPIO.PWM(led, 100)
+brightness.start(0)
 setup = '/sys/class/gpio/gpio2/direction'
 try:
     with open(setup) as f:
@@ -53,16 +54,11 @@ except IOError:
                             , shell=True)
     logging.info('Script has been ran from cron automatically')
 
-log = '/fan/log/log.log'
-# target temp
-temp = 42
-# how many breaches of the temp before turning fan on? (higher = longer)
-tries = 5
-temp = int(temp) * 0.935
-multi = 100 / (int(tries) + 2)
-i = 0
-off = 0
-status = 'off'
+targetFilePath = '/sys/class/gpio/gpio2/value'
+targetFile = open(targetFilePath, 'w')
+targetFile.write('0')
+targetFile.close()
+
 try:
     while True:
         currentTemp = \
@@ -73,8 +69,7 @@ try:
         targetFile = open(targetFilePath, 'w')
         fanStatusFile = 'status'
         fanStatus = open(fanStatusFile, 'w')
-        brightness.start(0)
-        brightnessLevel = 0
+        #brightnessLevel = 0
         if int(currentTemp) >= int(temp):
             off = 0
             i += 1
@@ -87,33 +82,41 @@ try:
             brightness.ChangeDutyCycle(brightnessLevel)
             logging.debug('Temp exceeded ' + str(int(currentTemp)) + ' - aiming for ' + str(int(temp)))
             if i > tries:
-                status = 'on'
-                targetFile.write('1')
-                targetFile.close()
-                fanStatus.write('on|' + str(int(currentTemp)))
-                fanStatus.close()
-                GPIO.output(16, GPIO.HIGH)
-                #brightness.ChangeDutyCycle(100)
-                logging.debug('Fan turned on ' + str(int(currentTemp)) + ' - aiming for ' + str(int(temp)))
+                if status == 'off':
+                    status = 'on'
+                    targetFile.write('1')
+                    targetFile.close()
+                    fanStatus.write('on|' + str(int(currentTemp)))
+                    fanStatus.close()
+                    brightness.ChangeDutyCycle(100)
+                    #brightness.ChangeDutyCycle(100)
+                    logging.debug('Fan TURNED ON ' + str(int(currentTemp)) + ' - aiming for ' + str(int(temp)))
+                else:
+                    logging.debug('Fan ALREADY ON ' + str(int(currentTemp)) + ' - aiming for ' + str(int(temp)))
                 i = int(i) - 1
         else:
             if status == 'on':
                 off += 1
-                logging.debug('Off try ' + str(int(off)))
                 if off >= tries:
                     #time.sleep(int(tries) * 5)
                     targetFile.write('0')
                     targetFile.close()
                     fanStatus.write('off|' + str(int(currentTemp)))
                     fanStatus.close()
-                    GPIO.output(16, GPIO.LOW)
-                    logging.debug('Fan turned off ' + str(int(currentTemp)) + ' - aiming for ' + str(int(temp)))
+                    #GPIO.output(16, GPIO.LOW)
+                    brightness.ChangeDutyCycle(0)
+                    logging.debug('Fan TURNED OFF ' + str(int(currentTemp)) + ' - aiming for ' + str(int(temp)))
                     off = 0
                     status = 'off'
+                    i = 0
+                else:
+                    logging.debug('Preparing to TURN OFF. ' + str(int(currentTemp)) + ' - aiming for ' + str(int(temp)))
+                    i = int(i) - 1
             else:
-                logging.debug('Fan not on and temperature has dropped. Iteration: ' + str(int(i)) + ' - Resetting back to 0 runs - ' + str(int(off)))
-                i = 0
+                logging.debug('Nothing to do. Iteration: ' + str(int(currentTemp)) + ' - aiming for ' + str(int(temp)))
                 off = 0
+                if i > 0:
+                    i = int(i) - 1
         time.sleep(5)
 except KeyboardInterrupt:
     GPIO.cleanup()
