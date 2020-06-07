4d0e3a11d583cfd54679a495ebff0cd211b9c271
PARAPAuk <parapa@gmail.com>
2020-06-07 18:19:42 +0100

    Removing some files

--- control.py
+++ control.py
@@ -16,7 +16,7 @@ import time, subprocess, logging, os, threading
 from datetime import datetime
 from pathlib import Path
 from rgb import rgb
-logging.basicConfig(filename='/fan/fan.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s',
+logging.basicConfig(filename='/fan/fan.log', level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 # csv location
